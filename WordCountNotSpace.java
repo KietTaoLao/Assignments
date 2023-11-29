5282f594ea1645948252c9d2853948f3dbefc9a2
KietTaoLao <82574415+KietTaoLao@users.noreply.github.com>
2023-11-29 15:34:16 +0700

    new quizz3

---
+++ WordCountNotSpace.java
@@ -0,0 +1,28 @@
+public class WordCountNotSpace {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("No input provided.");
+            return;
+        }
+
+        String input = args[0];
+        int wordCount = 0;
+        boolean isWord = false;
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+
+		if (ch >= '0' && ch <= '9') {
+			continue;
+		}
+		else if (ch ==' '){
+			continue;
+		}	
+		 else {
+                	wordCount++;
+            }
+        }
+
+        System.out.println("Number of words: " + wordCount);
+    }
+}
